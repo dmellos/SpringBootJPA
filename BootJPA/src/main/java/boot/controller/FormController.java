08b8586804f83fbb160804df34716ecf233eccb1
SELWYN DMELLO <dmelloselwyn@gmail.com>
2018-05-01 02:23:50 -0400

    Testing

--- BootJPA/src/main/java/boot/controller/FormController.java
+++ BootJPA/src/main/java/boot/controller/FormController.java
@@ -1,14 +1,13 @@
 package boot.controller;
 
-import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import boot.dao.TaskRepo;
-import boot.model.Task;
 import boot.service.TaskService;
 
 @RestController
@@ -17,10 +16,10 @@ public class FormController {
 	@Autowired
 	private TaskService taskService;
 	
-	
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello World";
+	@GetMapping("/ello")
+	public String hello(HttpServletRequest request) {
+		request.setAttribute("mode", "MODE_HOME");
+		return "index";
 	}
 	
 //	@GetMapping("/all-tasks")
--- BootJPA/src/main/webapp/WEB-INF/jsp/index.jsp
+++ BootJPA/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,5 +1,4 @@
 <!DOCTYPE HTML>
-
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <html>
